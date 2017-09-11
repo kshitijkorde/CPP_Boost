7ed2b1c299b1b0f6b5f87b507cb583f73b84d642
kshitijkorde <kshi.korde@gmail.com>
2017-09-11 14:13:11 +0100

    Recursive search inside directory for files and directories

--- Filesystem/Path.cpp
+++ Filesystem/Path.cpp
@@ -4,19 +4,33 @@
 using namespace std;
 using namespace boost::filesystem;
 
-int main(){
-
-	string FileName("/home/kshi/CPP_Boost/Filesystem/");
-	path P(FileName);	
-	if(exists(P)){
-		cout << "Yes " << FileName  << "Exist !!" << endl;
+void TraverseDirectory(path P, string DirName){
+	cout << "Parent:" << DirName << endl;
+	for(directory_iterator itr(P); itr != directory_iterator(); ++itr){
+		if(is_directory(status(itr->path()))){
+			string childdir = DirName+"/"+itr->path().filename().string();
+			cout << "Dir:" << childdir << endl;
+			path ChildPath(childdir);
+			TraverseDirectory(ChildPath, childdir);
+		}else if(is_regular_file(status(itr->path()))){
+			cout << "File: " << DirName+"/"+itr->path().filename().string() << endl;
+		}
 	}
+}
 
-	if(is_directory(status(P))){
-		cout << FileName << " is a directory" << endl;
-		cout << "List of files" << endl;
-		for(directory_iterator itr(P); itr != directory_iterator(); ++itr){
-			cout << itr->path().filename() << endl;
+int main(int argc, char * argv[]){
+
+	if(argc < 1){
+		cout << "Directory name not specified in the arguments list" << endl;
+		return 0;
+	}
+	string DirName(argv[1]);
+	path P(DirName);
+	if(exists(P)){
+		if(is_directory(status(P))){
+			TraverseDirectory(P, DirName);
+		} else {
+			cout << DirName << " is not a directory " << endl;
 		}
 	}
 	return 0;
